dcba05ac67776a517ed42df06abb567fa9f1340e
sukkisu <sukkisu88@gmail.com>
2025-03-03 13:54:00 +0900

    어노테이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,12 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
-
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -13,6 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
