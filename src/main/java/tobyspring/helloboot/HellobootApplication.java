7901ad191ad2620b3e12b9e95ba88999d6f0dcf4
sukkisu <sukkisu88@gmail.com>
2025-03-03 13:43:31 +0900

    DispatcherServlet으로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -22,44 +24,16 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			servletContext.addServlet("hello", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어, 공통 기능
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-					else{
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*");
-
-//			servletContext.addServlet("hello2", new HttpServlet() {
-//				@Override
-//				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
-//					String name = req.getParameter("name");
-//					resp.setStatus(HttpStatus.OK.value());
-//					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-//					resp.getWriter().println("Hello2 "+name);
-//				}
-//			}).addMapping("/hello2");
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext)
+					).addMapping("/*");
         });
 		webServer.start();
 
