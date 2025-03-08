18d05add3831ab48444f9e0fab5f06c999e304ec
sukkisu <sukkisu88@gmail.com>
2025-03-08 15:52:46 +0900

    메타 애노테이션과 합성 애노테이션

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -3,6 +3,22 @@ package tobyspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest{
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest{
+}
 public class HelloServiceTest {
 
     @Test
@@ -11,7 +27,7 @@ public class HelloServiceTest {
 
         String ret = helloService.sayHello("Test");
 
-        Assertions.assertThat(ret).isEqualTo("Hello Test");
+        Assertions.assertThat(ret).isEqualTo("^^Hello Test");
     }
 
     @Test
@@ -22,6 +38,7 @@ public class HelloServiceTest {
 
         Assertions.assertThat(ret).isEqualTo("*Test*");
     }
+
 }
 
 
